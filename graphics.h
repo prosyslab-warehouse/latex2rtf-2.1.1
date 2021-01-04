c2781f4caec1c31194f47fb9833c310cffefa54d
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-01-04 11:30:02 +0900

    init

---
+++ graphics.h
@@ -0,0 +1,29 @@
+#ifndef _INCLUDED_GRAPHICS_H
+#define _INCLUDED_GRAPHICS_H 1
+
+#define FIGURE_INCLUDEGRAPHICS 1
+#define FIGURE_EPSFFILE        2
+#define FIGURE_EPSFBOX         3
+#define FIGURE_BOXEDEPSF       4
+#define FIGURE_PSFIG           5
+
+void PrepareDisplayedBitmap(char *the_type);
+void FinishDisplayedBitmap(void);
+void WriteLatexAsBitmap(char *pre, char *eq, char *post);
+void PutLatexFile(const char *tex_file_stem, double scale, const char *pre);
+void CmdGraphics(int code);
+void CmdPicture(int code);
+void CmdPsPicture(int code);
+void CmdPsGraph(int code);
+void CmdMusic(int code);
+void CmdPsset(int code);
+void CmdNewPsStyle(int code);
+void CmdGraphicsPath(int code);
+
+typedef void PutFileFnc(char *, double, double, double, double);
+
+typedef struct {
+    char       *extension;
+    PutFileFnc *encoder;
+} GraphConvertElement;
+#endif
---
+++ vertical.h
@@ -0,0 +1,51 @@
+#define FIRST_PARAGRAPH           1
+#define GENERIC_PARAGRAPH         2
+#define SECTION_TITLE_PARAGRAPH   3
+#define EQUATION_PARAGRAPH        4
+
+#define INDENT_NONE    1
+#define INDENT_INHIBIT 2
+#define INDENT_USUAL   3
+
+#define MODE_INTERNAL_VERTICAL     1
+#define MODE_HORIZONTAL            2
+#define MODE_RESTRICTED_HORIZONTAL 3
+#define MODE_MATH                  4
+#define MODE_DISPLAYMATH           5
+#define MODE_VERTICAL              6
+
+extern char TexModeName[7][25];
+
+
+#define VSPACE_VSPACE     -1
+#define VSPACE_VSKIP       0
+#define VSPACE_SMALL_SKIP  1
+#define VSPACE_MEDIUM_SKIP 2
+#define VSPACE_BIG_SKIP    3
+
+void CmdBeginEnd(int code);
+void CmdEndParagraph(int code);
+void CmdIndent(int code);
+void CmdVspace(int code);
+void CmdDoubleSpacing(int code);
+void startParagraph(const char *style, int indenting);
+void CmdIndent(int code);
+void CmdNewPage(int code);
+void CmdAlign(int code);
+void CmdHfill(int code);
+
+void setLeftMarginIndent(int indent);
+void setRightMarginIndent(int indent);
+
+int  getLeftMarginIndent(void);
+int  getRightMarginIndent(void);
+
+void setAlignment(int align);
+int  getAlignment(void);
+
+int  getTexMode(void);
+void setTexMode(int mode);
+void changeTexMode(int mode);
+
+void setVspace(int space);
+int  getVspace(void);
